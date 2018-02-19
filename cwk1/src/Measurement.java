59ce05169be037fd37e23962905343e0b9b164bd
Slavyana Chervenkondeva [sc17sdc] <sc17sdc@comp-pc6140.leeds.ac.uk>
2018-02-19 16:19:25 +0000

    Add comments

--- cwk1/src/Measurement.java
+++ cwk1/src/Measurement.java
@@ -8,12 +8,14 @@ import java.time.format.DateTimeFormatter;
  *
  * @author Slavyana Dianova Chervenkondeva
  */
+
 public class Measurement {
   // Use this when parsing measurement time
   private static DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
   private LocalDateTime time;
   private int level;
 
+  // Measurement constructor
   public Measurement(String record) throws DataException{
       String[] data = record.split(",");
       switch (data.length) {
@@ -26,19 +28,21 @@ public class Measurement {
           default:
               throw new DataException("Invalid number of elements in record!");
       }
-
       String dateAndTime = data[0] + " " + data[1];
       time = LocalDateTime.parse(dateAndTime, FORMAT);
   }
 
+  // Returns the time of the Measurement
   public LocalDateTime getTime() {
     return time;
   }
 
+  // Returns the level of NO2
   public int getLevel() {
     return level;
   }
 
+  //Converts the Measurement to String
   @Override
   public String toString() {
     if(level == -1) return String.format("%s, no data", time);
--- cwk1/src/PollutionDataset.java
+++ cwk1/src/PollutionDataset.java
@@ -13,17 +13,19 @@ import java.util.Scanner;
  *
  * @author Slavyana Dianova Chervenkondeva
  */
+
 public class PollutionDataset {
    private ArrayList<Measurement> dataset;
 
   public PollutionDataset() {
-    dataset = new ArrayList();
+    dataset = new ArrayList<>();
   }
 
+  // Reads the CSV file, if it exists
   public void readCSV(String filename) throws FileNotFoundException {
     dataset.clear();
     Scanner input = new Scanner(new File(filename));
-    input.nextLine(); // skip first line
+    input.nextLine(); // Skips first line
     while(input.hasNextLine()){
       String record = input.nextLine();
       Measurement m = new Measurement(record);
@@ -32,14 +34,17 @@ public class PollutionDataset {
     input.close();
   }
 
+  // Adds a Measurement to the dataset.
   public void add(Measurement m) {
     dataset.add(m);
   }
 
+  // Returns the size of the dataset.
   public int size() {
     return dataset.size();
   }
 
+  // Returns the Measurement at a given index, if it exists
   public Measurement get(int index) {
     if(dataset.size() == 0 || dataset.size() < index + 1){
       throw new DataException("help");
@@ -48,22 +53,24 @@ public class PollutionDataset {
     else return dataset.get(index);
   }
 
+  // Returns the Measurement with the largest level of NO2
   public Measurement maxLevel() {
     if(dataset.size() == 0) {
       throw new DataException("help");
     }
-    int max = 0;
+
+    int maxLevel = 0;
     Measurement maxM = get(0);
       for (int i = 0; i < dataset.size(); i++) {
           Measurement m = get(i);
-          if(m.getLevel() > max){
-            max = m.getLevel();
+          if(m.getLevel() > maxLevel){
+            maxLevel = m.getLevel();
             maxM = m;
           }
       }
     return maxM;
   }
-  
+
    public Measurement minLevel()  throws DataException{
     if(dataset.size() == 0) {
       throw new DataException("help");
@@ -79,7 +86,7 @@ public class PollutionDataset {
       }
     return minM;
   }
-   
+
     public double meanLevel()  throws DataException{
     if(dataset.size() == 0) {
       throw new DataException("help");
