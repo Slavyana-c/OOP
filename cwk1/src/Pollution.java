fb5add9be72f277379e931826e4330a1b664730c
Slavyana Chervenkondeva [sc17sdc] <sc17sdc@comp-pc6132.leeds.ac.uk>
2018-02-22 16:52:25 +0000

    Scrapped changes to Pollution.java. PollutionPlot is a separate program.

--- cwk1/src/Pollution.java
+++ cwk1/src/Pollution.java
@@ -15,20 +15,20 @@ class Pollution {
     public static void main(String[] args) throws FileNotFoundException {
 
         PollutionDataset set = new PollutionDataset();
-        
+
         // Read the file path from command line
         Scanner in;
         if(args.length > 0) {
           in = new Scanner(args[0]);
         }
-        // Input file path, if no args were found 
+        // Input file path, if no args were found
         else {
           System.out.print("Input file path: ");
           in = new Scanner(System.in);
         }
           String fileName = in.nextLine();
 
-        // Read CSV file and print data, unless exceptions occur  
+        // Read CSV file and print data, unless exceptions occur
         try{
             set.readCSV(fileName);
 
@@ -41,38 +41,16 @@ class Pollution {
             }
             else System.out.printf("The EU rules were breached on %s.%n",
                     set.dayRulesBreached());
-            
-            
-            // Print a bar chart with the average monthly levels
-            System.out.print("Would you like to see a bar chart of the data? (y/n): ");
-            String choice = "";
-           
-            while (!(choice.equals("y") || choice.equals("n"))) {
-                choice = in.nextLine();
-                choice = choice.toLowerCase();
-                
-                switch (choice) {
-                    case "y":
-                        Application.launch(PollutionPlot.class, fileName);
-                        break;
-                    case "n":
-                        System.out.println("Ending program.");
-                        break;
-                    default:
-                        System.out.print("Invalid command! Please select (y/n): ");
-                        break;
-                }
-            }
         }
-        
+
         catch(FileNotFoundException e){
             System.out.println("File not found.");
         }
-        
+
         catch(Exception e){
             System.out.println("There was an error reading data from the file.");
             System.out.println("Please choose a different file.");
         }
-        
+
     }
 }
