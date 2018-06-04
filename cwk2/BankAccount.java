6cbac623a036af7ad727576897ea194b4d631b26
Slavyana Chervenkondeva <sc17sdc@leeds.ac.uk>
2018-06-04 10:45:13 +0300

    Changed BankAccount.java

---
+++ cwk2/BankAccount.java
@@ -0,0 +1,88 @@
+
+/**
+ * A single bank account
+ *
+ * <p>
+ * Created for COMP1721 Coursework 2.</p>
+ *
+ * @author Slavyana Dianova Chervenkondeva
+ */
+public class BankAccount {
+
+    protected int id;
+    protected String name;
+    protected int balance;
+
+    // BankAccount constructors
+    public BankAccount(int id, String name) {
+        this(id, name, 0);
+    }
+
+    public BankAccount(int id, String name, int bal) {
+        this.id = id;
+        this.name = name;
+        this.balance = 0;
+        if (bal != 0) {
+            deposit(bal);
+        }
+    }
+
+    // Sets the account id
+    public void setId(int i) {
+        id = i;
+    }
+
+    // Sets the account holder's name
+    public void setName(String n) {
+        name = n;
+    }
+
+    // Sets the account balance
+    public void setBalance(int bal) {
+        balance = bal;
+    }
+
+    // Returns the account id
+    public int getId() {
+        return id;
+    }
+
+    // Returns the account name
+    public String getName() {
+        return name;
+    }
+
+    // Returns the account balance
+    public int getBalance() {
+        return balance;
+    }
+
+    // Deposits money into the account if the amount is greater than 0
+    public void deposit(int amount) throws IllegalArgumentException {
+        if (amount > 0) {
+            balance += amount;
+
+        } else {
+            throw new IllegalArgumentException("Invalid amount");
+        }
+    }
+
+    // Withdraws money from the account, if there is enough
+    public void withdraw(int amount) throws IllegalArgumentException {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+
+        } else {
+            throw new IllegalArgumentException("Invalid amount");
+        }
+    }
+
+    // Returns a string with the account information
+    @Override
+    public String toString() {
+        return String.format("Account type: Bank Account\n"
+                + "Name: %s\n"
+                + "ID: %d\n"
+                + "Balance: %d\n", name, id, balance);
+    }
+}
